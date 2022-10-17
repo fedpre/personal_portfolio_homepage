fec57904ef86b95e63bc46993d4cc37dae91f7af
fedpre <fed.pregnolato@gmail.com>
2022-10-17 18:33:34 +0200

    Working on getting the 3d to work

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,22 +1,26 @@
-import Head from 'next/head';
+import Head from 'next/head'
 import Navbar from '../navbar'
 import { Box, Container } from '@chakra-ui/react'
+import VoxelLion from '../voxel-lion'
+import NoSsr from '../no-ssr'
 
 const Main = ({ children, router }) => {
-    return (
-        <Box as="main" pb={8}>
-            <Head>
-                <meta name='viewport' content='width=device-width, initial-scale=1' />
-                <title>Federico Pregnolato - Homepage</title>
-            </Head>
+  return (
+    <Box as="main" pb={8}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Federico Pregnolato - Homepage</title>
+      </Head>
 
-            <Navbar path={router.asPath} />
-
-            <Container maxW="container.md" pt={14}>
-                { children }
-            </Container>
-        </Box>
-    )
+      <Navbar path={router.asPath} />
+      <Container maxW="container.md" pt={14}>
+        <NoSsr>
+          <VoxelLion />
+        </NoSsr>
+        {children}
+      </Container>
+    </Box>
+  )
 }
 
 export default Main
